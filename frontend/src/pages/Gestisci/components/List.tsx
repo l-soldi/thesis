f52af1ea63bcbd8ff90b0fd4b6537344bb6799ec
Linda Soldi <linda.soldi@edreamsodigeo.com>
2024-12-03 23:06:30 +0100

    FE: inserimento componente Table

--- frontend/src/pages/Gestisci/components/List.tsx
+++ frontend/src/pages/Gestisci/components/List.tsx
@@ -1,24 +1,51 @@
 import { useLoaderData } from 'react-router-dom'
 import { FullReservation } from '../../../api/types'
 import Empty from './Empty'
-import ExperienceRecap from './ExperienceRecap'
+import Table from '../../../components/Table'
+import { useContext, useState } from 'react'
+import { ModalContext } from '../../../state/Modal'
+import { ModalTypes } from '../../../components/Modal/types'
+import { useApi } from '../../../api/hooks/useApi'
+import { deleteReservation, updateReservation } from '../../../api/methods'
+import { Modal } from '../../../components'
 
-type Props = {
-    handleSelected: (val: number) => void,
-}
-
-const List = ({handleSelected }: Props) => {
+const List = () => {
     const data = useLoaderData() as FullReservation[]
+    const [defaultValues, setDefaultValues] = useState<FullReservation | null>(null)
 
-    return (
-    <div className="detail">
-        {!data.length
-            ? <Empty />
-            : <div className='exp-list'>
-                {data.map(elem => <ExperienceRecap key={`exp-recap${elem.id}`} {...elem} handleSelected={() => handleSelected(elem.id)}/>)}
-              </div>
+    const columns = ['Nome', 'Cognome', 'Email', 'Esperienza', 'Data', 'Partecipanti', 'Prezzo']
+
+    const { showModal } = useContext(ModalContext)
+    const [modalType, setModalType] = useState<ModalTypes | null>(null)
+  
+    const ctaUpdate = useApi((values) => updateReservation(defaultValues!.id, values))
+    const ctaDelete = useApi(() => deleteReservation(defaultValues!.id), `/gestisci`, true)
+  
+    const handleCTA = (type: ModalTypes, defValues: FullReservation) => {
+      showModal()
+      setModalType(type)
+      setDefaultValues(defValues)
+    }
+  
+    const cta = modalType === ModalTypes.EDIT
+      ? ctaUpdate
+      : modalType === ModalTypes.DELETE
+        ? ctaDelete
+        : undefined
+
+    return (<>
+    <Modal type={modalType} cta={cta} defaultValues={defaultValues}/>
+        {
+            !data
+                ? <Empty />
+                : <Table
+                    rows={data.map(({ name, lastname, email, experience: { title }, date, peopleNum, totalPrice }) => [name, lastname, email, title, date, peopleNum.toString(), totalPrice.toString()])}
+                    columns={columns}
+                    onEdit={(index) => {handleCTA(ModalTypes.EDIT, data[index])}}
+                    onDelete={(index) => {handleCTA(ModalTypes.DELETE, data[index])}} 
+                />
         }
-    </div>
+    </>
   )
 }
 
